78ae6336f97252b2ed133dad862e32ea7f8c3c45
Sourabh Shirhatti <shirhatti@outlook.com>
2017-02-06 23:15:31 -0800

    Install VC++ redist

--- setup.ps1
+++ setup.ps1
@@ -4,6 +4,16 @@ Install-WindowsFeature -Name Web-WHC
 
 $tempFile = [System.IO.Path]::GetTempFileName() |
     Rename-Item -NewName { $_ -replace 'tmp$', 'exe' } -PassThru
+Invoke-WebRequest -Uri https://download.microsoft.com/download/6/A/A/6AA4EDFF-645B-48C5-81CC-ED5963AEAD48/vc_redist.x64.exe -OutFile $tempFile
+
+$logFile = [System.IO.Path]::GetTempFileName()
+
+$proc = (Start-Process $tempFile -PassThru "/quiet /install /log $logFile")
+$proc | Wait-Process
+Get-Content $logFile
+
+$tempFile = [System.IO.Path]::GetTempFileName() |
+    Rename-Item -NewName { $_ -replace 'tmp$', 'exe' } -PassThru
 Invoke-WebRequest -Uri http://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/DotNetCore.1.0.0-WindowsHosting.exe -OutFile $tempFile
 
 $logFile = [System.IO.Path]::GetTempFileName()
